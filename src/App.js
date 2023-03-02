9aa2675c508cb7bc0fd6ae3c4dd1f0cfce4ec1f9
Steve Willsmore <scwillsmore@gmail.com>
2023-03-02 12:18:30 -0500

    Add html structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main class="wrapper">
+      <header>
+        <div class="headerFlex">
+          <h1>PICaCOLOUR</h1>
+          <h2>An image search with a colourful twist</h2>
+        </div>
+
+        <form action="#">
+          <div class="searchBar">
+            <label for="searchInput">I want to see images of</label>
+            <input type="text" placeholder="cities" id="searchInput" required />
+          </div>
+
+          <div class="colourSelector">
+            <label for="colourSelection">that have a lot of</label>
+            <select
+              name="colourSelection"
+              class="colourSelection"
+              id="colourSelection"
+              autocomplete="off"
+              required
+            >
+              <option value="" selected disabled hidden>
+                choose colour
+              </option>
+            </select>
+            <input type="submit" value="Search" />
+          </div>
+        </form>
       </header>
-    </div>
+
+      <section class="results"></section>
+
+      <footer>
+        <p>
+          Created by
+          <a href="https://github.com/willsmores" target="_blank">
+            @willsmores
+          </a>
+          <br />
+          and powered by
+          <a href="https://pixabay.com/api/docs/" target="_blank">
+            Pixabay API
+          </a>
+        </p>
+      </footer>
+    </main>
   );
 }
 
